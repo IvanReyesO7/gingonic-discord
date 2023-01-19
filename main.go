d9203ae20894c5e652f579f2f6c90b02d428f2e0
Ivan Reyes <ivanwilf@hotmail.com>
2023-01-19 17:05:53 +0900

    Getting params passed through POST

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func postToDiscordWebHook(params map[string]string) {
+	fmt.Println(params)
+}
+
 func main() {
 	server := gin.Default()
 	server.LoadHTMLGlob("templates/*")
-	server.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	server.POST("/post-to-webhook", func(c *gin.Context) {
+
+		params := make(map[string]string)
+
+		params["webhook_url"] = c.PostForm("webhook_url")
+		params["username"] = c.PostForm("username")
+		params["content"] = c.PostForm("content")
+		params["avatar_url"] = c.PostForm("avatar_url")
+
+		postToDiscordWebHook(params)
 	})
 
 	server.GET("/index", func(c *gin.Context) {
