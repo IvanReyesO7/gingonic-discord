ad141d73e080972d0d47657146337634d249d9cb
Ivan Reyes <ivanwilf@hotmail.com>
2023-01-20 17:35:21 +0900

    Added cookie package to gin gonic

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 export GIN_MODE=release
 go mod init gingonic-discord
 go get github.com/gin-gonic/gin
+go get github.com/gin-contrib/sessions/cookie
 go build -tags netgo -ldflags '-s -w' -o gingonic-discord
