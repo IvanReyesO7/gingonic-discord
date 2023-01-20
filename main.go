fe9f7fc47e3b7863cda602452dfad98f7c038786
Ivan Reyes <ivanwilf@hotmail.com>
2023-01-20 17:34:19 +0900

    post function returns success or error

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	Discord "gingonic-discord/discord"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	server := gin.Default()
 
 	server.Static("/assets", "./assets")
 	server.LoadHTMLGlob("templates/index.html")
+	store := cookie.NewStore([]byte("secret"))
+	server.Use(sessions.Sessions("session", store))
 
 	server.POST("/post-to-webhook", func(c *gin.Context) {
+		session := sessions.Default(c)
 
 		webhook_url := c.PostForm("webhook_url")
 		params := make(map[string]string)
@@ -23,11 +30,23 @@ func main() {
 		params["content"] = c.PostForm("content")
 		params["avatar_url"] = c.PostForm("avatar_url")
 
-		Discord.PostToDiscordWebHook(webhook_url, params)
+		response := Discord.PostToDiscordWebHook(webhook_url, params)
+
+		if response != "" {
+			fmt.Println(response)
+			session.AddFlash(response)
+			session.Save()
+			c.JSON(http.StatusInternalServerError, "")
+		} else {
+			c.JSON(http.StatusOK, "")
+		}
 	})
 
 	server.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
+		session := sessions.Default(c)
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"alert": session.Flashes(),
+		})
 	})
 
 	server.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
